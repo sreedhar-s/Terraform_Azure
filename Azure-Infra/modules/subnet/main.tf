b1edeadb26185a9e8715d8bc4f27703645291c62
Gandikota Sreedhar <gandikota.sreedhar@olamnet.com>
2026-05-17 20:24:51 +0530

    updated subnet module

--- Azure-Infra/modules/subnet/main.tf
+++ Azure-Infra/modules/subnet/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_subnet" "snet" {
   for_each = var.snet
-  name                 = value.name
-  resource_group_name  = value.rg_name
-  virtual_network_name = value.vnt_name
-  address_prefixes     = value.address_prefixes
+  name                 = each.value.name
+  resource_group_name  = each.value.rg_name
+  virtual_network_name = each.value.vnt_name
+  address_prefixes     = each.value.address_prefixes
 }
