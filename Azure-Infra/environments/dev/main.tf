6746a61c9725a7873541a4a800e05233d26c514e
Gandikota Sreedhar <gandikota.sreedhar@olamnet.com>
2026-05-18 11:06:17 +0530

    updated main.tf file

--- Azure-Infra/environments/dev/main.tf
+++ Azure-Infra/environments/dev/main.tf
@@ -18,11 +18,13 @@ module "subnet" {
 module "virtual_network_peering" {
     source = "../../modulles/virtual_network_peering"
     local_to_remote = var.local_to_remote
+    remote_vnt_id = data.azurerm_virtual_network.remote_vnt.id
     depends_on = [ module.subnet ]
 }
 
 module "virtual_network_peering" {
     source = "../../modulles/virtual_network_peering"
     remote_to_local = var.remote_to_local
+    local_vnt_id = module.virtual_network.vnt[vnt].id
     depends_on = [ module.subnet ]
 }
