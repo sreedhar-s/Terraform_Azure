b5c567d41236bcc4665d597cccdd112c21fa2bd9
Gandikota Sreedhar <gandikota.sreedhar@olamnet.com>
2026-05-20 01:18:22 +0530

    updated main.tf file

--- Azure-Infra/environments/dev/main.tf
+++ Azure-Infra/environments/dev/main.tf
@@ -18,9 +18,9 @@ module "subnet" {
 module "spoke_to_hub_peering" {    
   source = "../../modules/virtual_network_peering"
   vnt_peering = {
-    name = "DT-DEV-VNT-TO-OI-VNT"
-    rg_name = "DT-VNT-RG"
-    vnt_name = "DT-DEV-VNT"
+    name = "${module.virtual_network.vnt_name["vnt"]}-TO-${data.azurerm_virtual_network.hub_vnt.name}"
+    rg_name = module.resource_group.rg_name["vnt_rg"]
+    vnt_name = module.virtual_network.vnt_name["vnt"]
     allow_virtual_network_access = true
     allow_forwarded_traffic = true
     allow_gateway_transit = false
@@ -37,14 +37,14 @@ module "hub_to_spoke_peering" {
   } 
   source = "../../modules/virtual_network_peering"
   vnt_peering = {
-    name = "DT-DEV-VNT-TO-OI-VNT"
-    rg_name = "DT-VNT-RG"
-    vnt_name = "DT-DEV-VNT"
+    name = "${data.azurerm_virtual_network.hub_vnt.name}-TO-${module.virtual_network.vnt_name["vnt"]}"
+    rg_name = data.azurerm_virtual_network.hub_vnt.resource_group_name
+    vnt_name = data.azurerm_virtual_network.hub_vnt.name
     allow_virtual_network_access = true
     allow_forwarded_traffic = true
-    allow_gateway_transit = false
-    use_remote_gateways = true
-    remote_vnt_id = module.virtual_network.vnt_id["DT-DEV-VNT"]
+    allow_gateway_transit = true
+    use_remote_gateways = false
+    remote_vnt_id = module.virtual_network.vnt_id["vnt"]
   }
 
   depends_on = [module.virtual_network]
