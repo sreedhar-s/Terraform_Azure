601f386f6e8ea3f30429ff61aa5bfccd6ffe8bf9
Gandikota Sreedhar <gandikota.sreedhar@olamnet.com>
2026-06-06 23:59:00 +0530

    updated dns record module

--- Azure-Infra/modules/dns_record/main.tf
+++ Azure-Infra/modules/dns_record/main.tf
@@ -1,8 +1,8 @@
 resource "azurerm_private_dns_a_record" "kvt_private_dns_record" {
   for_each = var.dns_record  
-  name                = var.dns_record.name
-  zone_name           = var.dns_record.zone_name
-  resource_group_name = var.dns_record.resource_group_name
-  ttl                 = var.dns_record.ttl
-  records             = [var.dns_record.records[0]]
+  name                = each.value.name
+  zone_name           = each.value.zone_name
+  resource_group_name = each.value.resource_group_name
+  ttl                 = each.value.ttl
+  records             = [each.value.records[0]]
 }
