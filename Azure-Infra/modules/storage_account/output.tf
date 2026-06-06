3285ffd238e61fee1c1a00a8a52a33814843b0ce
Gandikota Sreedhar <gandikota.sreedhar@olamnet.com>
2026-06-06 19:51:47 +0530

    updated storage account module

--- Azure-Infra/modules/storage_account/output.tf
+++ Azure-Infra/modules/storage_account/output.tf
@@ -1,13 +1,13 @@
 output "stg_id" {
   description = "The ID of the created storage account"
     value       = {
-        for k, v in azurerm_storage_account.stg_acc.id : k => v.id
+        for k, v in azurerm_storage_account.stg_acc : k => v.id
     }
 }
 
 output "primary_blob_endpoint" {
   description = "The primary blob endpoint of the created storage account"
     value       = {
-        for k, v in azurerm_storage_account.stg_acc.primary_blob_endpoint: k => v.primary_blob_endpoint
+        for k, v in azurerm_storage_account.stg_acc : k => v.primary_blob_endpoint
     }
 }
