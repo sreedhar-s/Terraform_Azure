829af7978d8014b9c032e6f8187e188dde1b01f4
Gandikota Sreedhar <gandikota.sreedhar@olamnet.com>
2026-06-06 20:37:46 +0530

    updated linux vm output file

--- Azure-Infra/modules/linux_vm/output.tf
+++ Azure-Infra/modules/linux_vm/output.tf
@@ -1,6 +1,6 @@
 output "vm_id" {
   description = "The ID of the created virtual machine"
     value       = {
-        for k, v in azurerm_windows_virtual_machine.windowsvm : k => v.id
+        for k, v in azurerm_linux_virtual_machine.linuxvm : k => v.id
     }
 }
