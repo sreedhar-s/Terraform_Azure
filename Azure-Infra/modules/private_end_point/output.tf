833b9030f2dbf7b82029521867b6994c0c8b7fe9
Gandikota Sreedhar <gandikota.sreedhar@olamnet.com>
2026-06-06 23:49:56 +0530

    updated pvl module

--- Azure-Infra/modules/private_end_point/output.tf
+++ Azure-Infra/modules/private_end_point/output.tf
@@ -1,6 +1,6 @@
 output "private_ip_address" {
   description = "The private_ip_address of the endpoint"
     value       = {
-        for k, v in azurerm_private_endpoint.kvt_private_endpoint.private_service_connection : k => v.private_ip_address
+        for k, v in azurerm_private_endpoint.kvt_private_endpoint : k => v.private_service_connection[0].private_ip_address
     }
 }
