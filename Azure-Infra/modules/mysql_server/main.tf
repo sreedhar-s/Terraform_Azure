254ce753e103d218573ca55127d0afabb7612ad2
Gandikota Sreedhar <gandikota.sreedhar@olamnet.com>
2026-06-07 12:27:20 +0530

    updated mysql server module

--- Azure-Infra/modules/mysql_server/main.tf
+++ Azure-Infra/modules/mysql_server/main.tf
@@ -1,17 +1,17 @@
 resource "azurerm_mysql_flexible_server" "mysql-flexible-server" {
   for_each = var.mysql-flexible-server  
-  name                   = each.value.mysql-flexible-server.name
-  resource_group_name    = each.value.mysql-flexible-server.resource_group_name
-  location               = each.value.mysql-flexible-server.location
-  administrator_login    = each.value.mysql-flexible-server.administrator_login
-  administrator_password = each.value.mysql-flexible-server.administrator_password
-  backup_retention_days  = each.value.mysql-flexible-server.backup_retention_days
-  sku_name               = each.value.mysql-flexible-server.sku_name
-  public_network_access = each.value.mysql-flexible-server.public_network_access
-  version = each.value.mysql-flexible-server.version
+  name                   = each.value.name
+  resource_group_name    = each.value.resource_group_name
+  location               = each.value.location
+  administrator_login    = each.value.administrator_login
+  administrator_password = each.value.administrator_password
+  backup_retention_days  = each.value.backup_retention_days
+  sku_name               = each.value.sku_name
+  public_network_access = each.value.public_network_access
+  version = each.value.version
 
   storage {
-    size_gb = each.value.mysql-flexible-server.storage.size_gb
-    auto_grow_enabled = each.value.mysql-flexible-server.storage.auto_grow_enabled
+    size_gb = each.value.storage.size_gb
+    auto_grow_enabled = each.value.storage.auto_grow_enabled
   }
 }
