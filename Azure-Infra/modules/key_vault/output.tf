e8f7501d0c3a90e4cc751a88f80357c0901cedbd
Gandikota Sreedhar <gandikota.sreedhar@olamnet.com>
2026-06-07 11:24:57 +0530

    updated key vault module

--- Azure-Infra/modules/key_vault/output.tf
+++ Azure-Infra/modules/key_vault/output.tf
@@ -2,6 +2,6 @@ output "kvt_id" {
     description = "The Id of the key vault"
 
     value = {
-        for k,v in azurem.azurerm_key_vault.keyvault : k => v.id
+        for k,v in azurerm_key_vault.keyvault : k => v.id
     }
 }
